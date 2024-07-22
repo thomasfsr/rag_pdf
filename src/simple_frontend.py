9ff1ff224a2e4d9f7e57a093094787e248de309a
Thomás Freire <thomas.fsr@gmail.com>
2024-07-22 15:43:16 -0300

    api link correction

--- src/simple_frontend.py
+++ src/simple_frontend.py
@@ -3,7 +3,8 @@ import httpx
 import asyncio
 
 FASTAPI_URL = "http://fastapi:8000/query"
-
+#FASTAPI_URL2 = "http://fastapi:8000/query"
+FASTAPI_URL2 = "http://localhost:8000/query"
 def run():
     # col1, col2 = st.columns([.2,.8], vertical_alignment='center')
     # with col1:
@@ -14,13 +15,22 @@ def run():
     query_text = st.text_input("Enter your query:")
 
     async def get_response(query_text):
-        async with httpx.AsyncClient(timeout=120.0) as client:
-            async with client.stream("POST", FASTAPI_URL, json={"query_text": query_text}) as response:
-                response_text = ""
-                async for chunk in response.aiter_text():
-                    response_text += chunk
-                    st.write(chunk)  # Stream response chunks to the app
-                return response_text
+        try:
+            async with httpx.AsyncClient(timeout=120.0) as client:
+                async with client.stream("POST", FASTAPI_URL, json={"query_text": query_text}) as response:
+                    response_text = ""
+                    async for chunk in response.aiter_text():
+                        response_text += chunk
+                        st.write(chunk)  # Stream response chunks to the app
+                    return response_text
+        except:
+            async with httpx.AsyncClient(timeout=120.0) as client:
+                async with client.stream("POST", FASTAPI_URL2, json={"query_text": query_text}) as response:
+                    response_text = ""
+                    async for chunk in response.aiter_text():
+                        response_text += chunk
+                        st.write(chunk)  # Stream response chunks to the app
+                    return response_text
 
     if st.button("Submit"):
         if query_text:
